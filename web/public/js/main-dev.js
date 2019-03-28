782a71e9c9cda0d7845d1fa374293c3239d9f9bb
Marcel Hauser <maha93@gmx.de>
2019-03-28 16:55:30 +0100

    barba fix

--- web/public/js/main-dev.js
+++ web/public/js/main-dev.js
@@ -6,32 +6,10 @@ var socket = io();
 
 // Dummy example to illustrate rules and hooks…
 barba.init({
+  debug: true,
   transitions: [{
-    name: 'dummy transitions',
-    // Apply only when leaving `[data-barba-namespace="home"]`.
-    namespace: 'home',
-    // Apply only when transitioning to `[data-barba-namespace="products | contact"]`.
-    to: {
-      namespace: ['products', 'contact']
-    },
-    // Apply only if clicked link contains `.cta`.
-    custom: function custom(_ref) {
-      var current = _ref.current,
-          next = _ref.next,
-          trigger = _ref.trigger;
-      return trigger.classList && trigger.classList.contains('cta');
-    },
-    // Do leave and enter concurrently.
-    sync: true,
-    // Available hooks…
-    beforeAppear: function beforeAppear() {},
-    appear: function appear() {},
-    afterAppear: function afterAppear() {},
-    beforeLeave: function beforeLeave() {},
-    leave: function leave() {},
-    afterLeave: function afterLeave() {},
-    beforeEnter: function beforeEnter() {},
-    enter: function enter() {},
-    afterEnter: function afterEnter() {}
+    name: 'home'
+  }, {
+    name: 'about'
   }]
 });
--- web/public/js/main.js
+++ web/public/js/main.js
@@ -4,30 +4,10 @@ var socket = io();
 
 // Dummy example to illustrate rules and hooks…
 barba.init({
-  transitions: [
-    {
-      name: 'dummy transitions',
-      // Apply only when leaving `[data-barba-namespace="home"]`.
-      namespace: 'home',
-      // Apply only when transitioning to `[data-barba-namespace="products | contact"]`.
-      to: {
-        namespace: ['products', 'contact'],
-      },
-      // Apply only if clicked link contains `.cta`.
-      custom: ({ current, next, trigger }) =>
-        trigger.classList && trigger.classList.contains('cta'),
-      // Do leave and enter concurrently.
-      sync: true,
-      // Available hooks…
-      beforeAppear() {},
-      appear() {},
-      afterAppear() {},
-      beforeLeave() {},
-      leave() {},
-      afterLeave() {},
-      beforeEnter() {},
-      enter() {},
-      afterEnter() {},
-    },
-  ],
+  debug: true,
+  transitions: [{
+    name: 'home',
+  }, {
+    name: 'about'
+  }],
 });
